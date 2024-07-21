43cf566befc20ebd534533125ff6e7f45a32388f
esmerquenn <esmereyvazov@gmail.com>
2024-07-21 13:59:13 +0400

    video end i guess

--- src/Components/VideoComponents/Rovshan.jsx
+++ src/Components/VideoComponents/Rovshan.jsx
@@ -1,6 +1,279 @@
+// import React, { useEffect, useRef, useState } from "react";
+// import useWidth from "../../Hooks/UseWidth";
+// const Rovshan = ({ isPlayed, setIsPlayed, setLoading, loading }) => {
+//   const width = useWidth();
+//   const videoRef = useRef();
+//   const [videoVisible, setVideoVisible] = useState(true);
+
+//   const splashDesktop = "/assets/video/splash-screen-desktop.webm";
+//   const loopDesktop = "/assets/video/desktop-loop.webm";
+//   const splashMobile = "/assets/video/splash-screen-mobile.webm";
+//   const loopMobile = "/assets/video/mobile-loop.webm";
+
+//   const handleCanPlayThrough = (videoElement, loop) => {
+//     videoElement.play();
+//     videoElement.playsInline = true;
+//     videoElement.loop = loop;
+//     setLoading(false);
+//     if (loop) {
+//       setTimeout(() => setVideoVisible(true), 5000); // 5 seconds delay to match sidebar
+//     }
+//   };
+
+//   useEffect(() => {
+//     const videoElement = videoRef.current;
+
+//     const setVideoSourceAndPlay = (src, loop) => {
+//       setLoading(true);
+//       setVideoVisible(false);
+//       videoElement.src = src;
+//       videoElement.load();
+//       videoElement.addEventListener("canplaythrough", () => handleCanPlayThrough(videoElement, loop), { once: true });
+//     };
+
+//     const handleVideoEnded = () => {
+//       sessionStorage.setItem("isPlayed", JSON.stringify(true));
+//       setIsPlayed(true);
+//       setVideoVisible(false);
+//       setTimeout(() => {
+//         const loopSrc = width > 548 ? loopDesktop : loopMobile;
+//         setVideoSourceAndPlay(loopSrc, true);
+//       }, 10);
+//     };
+
+//     if (width > 548) {
+//       if (isPlayed) {
+//         setVideoSourceAndPlay(loopDesktop, true);
+//         setVideoVisible(true);
+//       } else {
+//         setVideoSourceAndPlay(splashDesktop, false);
+//         videoElement.addEventListener("ended", handleVideoEnded, { once: true });
+//       }
+//     } else {
+//       if (isPlayed) {
+//         setVideoSourceAndPlay(loopMobile, true);
+//         setVideoVisible(true);
+//       } else {
+//         setVideoSourceAndPlay(splashMobile, false);
+//         videoElement.addEventListener("ended", handleVideoEnded, { once: true });
+//       }
+//     }
+
+//     return () => {
+//       videoElement.removeEventListener("ended", handleVideoEnded);
+//     };
+//   }, [setIsPlayed]);
+
+//   return (
+//     <div className="videobg-container">
+//       {loading && !isPlayed ? (
+//         <div className="loading">
+//           <div className="loader_login"></div>
+//         </div>
+//       ) : null}
+//       <video
+//         ref={videoRef}
+//         autoPlay
+//         className={`video_bg video-element ${videoVisible ? "visible" : "hidden"}`}
+//         playsInline
+//         preload="auto"
+//         muted
+//         style={{ display: "block" }}
+//       ></video>
+//     </div>
+//   );
+// };
+
+// export default Rovshan;
+// import React, { useEffect, useRef, useState } from "react";
+// import useWidth from "../../Hooks/UseWidth";
+
+// const Rovshan = ({ isPlayed, setIsPlayed, setLoading, loading }) => {
+//   const width = useWidth();
+//   const videoRef = useRef();
+//   const [videoVisible, setVideoVisible] = useState(false);
+
+//   const splashDesktop = "/assets/video/splash-screen-desktop.webm";
+//   const loopDesktop = "/assets/video/desktop-loop.webm";
+//   const splashMobile = "/assets/video/splash-screen-mobile.webm";
+//   const loopMobile = "/assets/video/mobile-loop.webm";
+
+//   const handleCanPlayThrough = (videoElement, loop) => {
+//     videoElement.play();
+//     videoElement.playsInline = true;
+//     videoElement.loop = loop;
+//     setLoading(false);
+
+//     if (loop) {
+//       videoElement.style.opacity = 0;
+//       setTimeout(() => {
+//         videoElement.style.transition = "opacity 5s";
+//         videoElement.style.opacity = 1;
+//       }, 500); // 50 ms delay to ensure the transition is applied correctly
+//     } else {
+//       videoElement.style.opacity = 1;
+//     }
+//   };
+
+//   useEffect(() => {
+//     const videoElement = videoRef.current;
+
+//     const setVideoSourceAndPlay = (src, loop) => {
+//       setLoading(true);
+//       videoElement.src = src;
+//       videoElement.load();
+//       videoElement.addEventListener("canplaythrough", () => handleCanPlayThrough(videoElement, loop), { once: true });
+//     };
+
+//     const handleVideoEnded = () => {
+//       sessionStorage.setItem("isPlayed", JSON.stringify(true));
+//       setIsPlayed(true);
+//       const loopSrc = width > 548 ? loopDesktop : loopMobile;
+//       setVideoSourceAndPlay(loopSrc, true);
+//     };
+
+//     if (width > 548) {
+//       if (isPlayed) {
+//         setVideoSourceAndPlay(loopDesktop, true);
+//       } else {
+//         setVideoSourceAndPlay(splashDesktop, false);
+//         videoElement.addEventListener("ended", handleVideoEnded, { once: true });
+//       }
+//     } else {
+//       if (isPlayed) {
+//         setVideoSourceAndPlay(loopMobile, true);
+//       } else {
+//         setVideoSourceAndPlay(splashMobile, false);
+//         videoElement.addEventListener("ended", handleVideoEnded, { once: true });
+//       }
+//     }
+
+//     return () => {
+//       videoElement.removeEventListener("ended", handleVideoEnded);
+//     };
+//   }, [isPlayed, setIsPlayed, width]);
+
+//   return (
+//     <div className="videobg-container">
+//       {loading && !isPlayed ? (
+//         <div className="loading">
+//           <div className="loader_login"></div>
+//         </div>
+//       ) : null}
+//       <video
+//         ref={videoRef}
+//         autoPlay
+//         className="video_bg video-element"
+//         playsInline
+//         preload="auto"
+//         muted
+//         style={{ display: "block" }}
+//       ></video>
+//     </div>
+//   );
+// };
+
+// export default Rovshan;
+
+// import React, { useEffect, useRef } from "react";
+// import useWidth from "../../Hooks/UseWidth";
+
+// const Rovshan = ({ isPlayed, setIsPlayed, setLoading, loading }) => {
+//   const width = useWidth();
+//   const videoRef = useRef();
+
+//   const splashDesktop = "/assets/video/splash-screen-desktop.webm";
+//   const loopDesktop = "/assets/video/desktop-loop.webm";
+//   const splashMobile = "/assets/video/splash-screen-mobile.webm";
+//   const loopMobile = "/assets/video/mobile-loop.webm";
+
+//   const handleCanPlayThrough = (videoElement, loop) => {
+//     if (loop) {
+//       videoElement.style.opacity = 0;
+//       setTimeout(() => {
+//         videoElement.style.transition = "opacity 5s ease-in-out";
+//         videoElement.style.opacity = 1;
+//       }, 1000);
+//     }
+//     else{
+//       videoElement.style.opacity = 1;
+
+//     }
+//     videoElement.play();
+//     videoElement.playsInline = true;
+//     videoElement.loop = loop;
+//     setLoading(false);
+//   };
+
+//   useEffect(() => {
+//     const videoElement = videoRef.current;
+//     const setVideoSourceAndPlay = (src, loop) => {
+//       console.log(videoElement.src,loop, 'src');
+//       if (loop) {
+//         videoElement.style.opacity = 0;
+//       } else {
+//         videoElement.style.opacity = 1;
+//       }
+//       videoElement.src = src;
+//       setLoading(true);
+//       videoElement.load();
+//       videoElement.addEventListener("canplaythrough", () => handleCanPlayThrough(videoElement, loop), { once: true });
+//     };
+
+//     const handleVideoEnded = () => {
+//       sessionStorage.setItem("isPlayed", JSON.stringify(true));
+//       setIsPlayed(true);
+//       const loopSrc = width > 548 ? loopDesktop : loopMobile;
+//       setVideoSourceAndPlay(loopSrc, true);
+//     };
+
+//     if (width > 548) {
+//       if (isPlayed) {
+//         setVideoSourceAndPlay(loopDesktop, true);
+//       } else {
+//         setVideoSourceAndPlay(splashDesktop, false);
+//         videoElement.addEventListener("ended", handleVideoEnded, { once: true });
+//       }
+//     } else {
+//       if (isPlayed) {
+//         setVideoSourceAndPlay(loopMobile, true);
+//       } else {
+//         setVideoSourceAndPlay(splashMobile, false);
+//         videoElement.addEventListener("ended", handleVideoEnded, { once: true });
+//       }
+//     }
+
+//     return () => {
+//       videoElement.removeEventListener("ended", handleVideoEnded);
+//     };
+//   }, [isPlayed, setIsPlayed, width]);
+
+//   return (
+//     <div className="videobg-container">
+//       {loading && !isPlayed ? (
+//         <div className="loading">
+//           <div className="loader_login"></div>
+//         </div>
+//       ) : null}
+//       <video
+//         ref={videoRef}
+//         autoPlay
+//         className="video_bg video-element"
+//         playsInline
+//         preload="auto"
+//         muted
+//         style={{ display: "block" }}
+//       ></video>
+//     </div>
+//   );
+// };
+
+// export default Rovshan;
+
 import React, { useEffect, useRef } from "react";
 import useWidth from "../../Hooks/UseWidth";
-const Rovshan = ({ isPlayed, setIsPlayed,setLoading , loading }) => {
+
+const Rovshan = ({ isPlayed, setIsPlayed, setLoading, loading, hasVisited }) => {
   const width = useWidth();
   const videoRef = useRef();
 
@@ -10,6 +283,12 @@ const Rovshan = ({ isPlayed, setIsPlayed,setLoading , loading }) => {
   const loopMobile = "/assets/video/mobile-loop.webm";
 
   const handleCanPlayThrough = (videoElement, loop) => {
+    if (loop) {
+      setTimeout(() => {
+        videoElement.style.transition = "opacity 4s ease-in-out";
+        videoElement.style.opacity = 1;
+      }, 10);
+    }
     videoElement.play();
     videoElement.playsInline = true;
     videoElement.loop = loop;
@@ -20,8 +299,10 @@ const Rovshan = ({ isPlayed, setIsPlayed,setLoading , loading }) => {
     const videoElement = videoRef.current;
 
     const setVideoSourceAndPlay = (src, loop) => {
-      setLoading(true);
+      videoElement.style.transition = "none"; // Remove any transition to set initial opacity
+      videoElement.style.opacity = hasVisited ? 1 : loop ? 0 : 1; // Ensure the first video is visible, second video starts hidden
       videoElement.src = src;
+      setLoading(true);
       videoElement.load();
       videoElement.addEventListener("canplaythrough", () => handleCanPlayThrough(videoElement, loop), { once: true });
     };
@@ -29,10 +310,8 @@ const Rovshan = ({ isPlayed, setIsPlayed,setLoading , loading }) => {
     const handleVideoEnded = () => {
       sessionStorage.setItem("isPlayed", JSON.stringify(true));
       setIsPlayed(true);
-      setTimeout(() => {
-        const loopSrc = width > 548 ? loopDesktop : loopMobile;
-        setVideoSourceAndPlay(loopSrc, true);
-      }, 10);
+      const loopSrc = width > 548 ? loopDesktop : loopMobile;
+      setVideoSourceAndPlay(loopSrc, true);
     };
 
     if (width > 548) {
@@ -54,7 +333,7 @@ const Rovshan = ({ isPlayed, setIsPlayed,setLoading , loading }) => {
     return () => {
       videoElement.removeEventListener("ended", handleVideoEnded);
     };
-  }, [setIsPlayed]);
+  }, [isPlayed, setIsPlayed, width, hasVisited]);
 
   return (
     <div className="videobg-container">
@@ -63,15 +342,15 @@ const Rovshan = ({ isPlayed, setIsPlayed,setLoading , loading }) => {
           <div className="loader_login"></div>
         </div>
       ) : null}
-      <video 
-      ref={videoRef}
-       autoPlay
-        className="video_bg video-element" 
-        playsInline 
-        preload="auto" 
+      <video
+        ref={videoRef}
+        autoPlay
+        className="video_bg video-element"
+        playsInline
+        preload="auto"
         muted
-        style={{display:"block"}}
-        ></video>
+        style={{ display: "block" }}
+      ></video>
     </div>
   );
 };
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,3 +1,45 @@
+// import React, { useEffect, useState } from "react";
+// import Sidebar from "../Components/SideBarComponents/Sidebar";
+// import Rovshan from "../Components/VideoComponents/Rovshan";
+
+// function Login() {
+//   const [toggle, setToggle] = useState(false);
+//   const [isPlayed, setIsPlayed] = useState(() => JSON.parse(sessionStorage.getItem("isPlayed")) || false);
+//   const [loading, setLoading] = useState(true);
+
+//   const handleToggle = () => {
+//     setToggle((prev) => !prev);
+//   };
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       setToggle(false);
+//     };
+
+//     window.addEventListener("scroll", handleScroll);
+
+//     return () => {
+//       window.removeEventListener("scroll", handleScroll);
+//     };
+//   }, []);
+
+//   useEffect(() => {
+//     console.log(isPlayed);
+//   }, [setIsPlayed]);
+//   return (
+//     <div className="login_me ">
+//       <div onClick={handleToggle} className={`sidebar_div container flex_container invisible ${isPlayed && !loading  ? "visible" : ""}`}>
+//         <Sidebar toggle={toggle} />
+//       </div>
+//       <div onClick={handleToggle} className={`blur_div esmer ${toggle ? "cover" : " "}`}></div>
+//       <div className="video_div">
+//         <Rovshan {...{ isPlayed, setIsPlayed, setLoading, loading }} />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Login;
+
 import React, { useEffect, useState } from "react";
 import Sidebar from "../Components/SideBarComponents/Sidebar";
 import Rovshan from "../Components/VideoComponents/Rovshan";
@@ -6,10 +48,12 @@ function Login() {
   const [toggle, setToggle] = useState(false);
   const [isPlayed, setIsPlayed] = useState(() => JSON.parse(sessionStorage.getItem("isPlayed")) || false);
   const [loading, setLoading] = useState(true);
+  const [hasVisited, setHasVisited] = useState(() => JSON.parse(sessionStorage.getItem("hasVisited")) || false);
 
   const handleToggle = () => {
     setToggle((prev) => !prev);
   };
+
   useEffect(() => {
     const handleScroll = () => {
       setToggle(false);
@@ -25,17 +69,26 @@ function Login() {
   useEffect(() => {
     console.log(isPlayed);
   }, [setIsPlayed]);
+
+  useEffect(() => {
+    if (isPlayed && !loading) {
+      setHasVisited(true);
+      sessionStorage.setItem("hasVisited", JSON.stringify(true));
+    }
+  }, [isPlayed, loading]);
+
   return (
     <div className="login_me ">
-      <div onClick={handleToggle} className={`sidebar_div container flex_container invisible ${isPlayed && !loading  ? "visible" : ""}`}>
+      <div onClick={handleToggle} className={`sidebar_div container flex_container invisible ${isPlayed && !loading || hasVisited ? "visible" : ""}`}>
         <Sidebar toggle={toggle} />
       </div>
       <div onClick={handleToggle} className={`blur_div esmer ${toggle ? "cover" : " "}`}></div>
       <div className="video_div">
-        <Rovshan {...{ isPlayed, setIsPlayed, setLoading, loading }} />
+        <Rovshan {...{ isPlayed, setIsPlayed, setLoading, loading, hasVisited }} />
       </div>
     </div>
   );
 }
 
 export default Login;
+
